7f3d67e34979ef0612060f0c09097793202c2d6d
Sean Foley <seanremy@gmail.com>
2015-08-27 15:05:38 -0400

    Update NNetwork.java

--- NNetwork.java
+++ NNetwork.java
@@ -29,4 +29,60 @@ public class NNetwork {
 		
 	}
 	
+	//fires all the input neurons and returns the outputs
+	public double[] evaluate(double[] input) {
+		
+		//check for appropriately sized input
+		if(input.length != inputLayer.getSize())
+			return null;
+		
+		//fire hidden layers
+		double[] activationFront = new double[0];
+		if(hiddenLayers.length > 0)
+			activationFront = hiddenLayers[0].evaluate(input);
+		for(int i = 1; i < hiddenLayers.length; i++) 
+			activationFront = hiddenLayers[i].evaluate(activationFront);
+		
+		//fire output layer
+		activationFront = outputLayer.evaluate(activationFront);
+		
+		return activationFront;
+		
+	}
+	
+	public int getNumLayers() {
+		return 2 + hiddenLayers.length;
+	}
+	
+	public NLayer getInputLayer() {
+		return inputLayer;
+	}
+	
+	public NLayer getOutputLayer() {
+		return outputLayer;
+	}
+	
+	public NLayer getLayer(int i) {
+		if(i == 0)
+			return inputLayer;
+		else if(i <= hiddenLayers.length)
+			return hiddenLayers[i - 1];
+		else if(i == hiddenLayers.length + 1)
+			return outputLayer;
+		else
+			return null;
+	}
+	
+	public String toString() {
+		
+		String ret = "";
+		ret.concat(inputLayer.toString() + "\n");
+		for(int i = 0; i < hiddenLayers.length; i++) {
+			ret.concat(hiddenLayers[i] + "\n");
+		}
+		ret.concat(outputLayer.toString());
+		return ret;
+		
+	}
+	
 }
