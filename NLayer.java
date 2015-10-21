3f0e490a9f59231db825532da53e4eac3af5dd99
Sean Foley <seanremy@gmail.com>
2015-10-20 22:31:14 -0400

    Update NLayer.java

--- NLayer.java
+++ NLayer.java
@@ -1,63 +1,72 @@
-
+//Represents a layer of neurons in an artificial neural network
 public class NLayer {
 
 	private Neuron[] neurons;
-	
-	//constructor creates a layer of 'size' neurons with 0 dendrites (input layer)
-	public NLayer(int size) {
-		this(size, 0);
-	}
-	
-	//constructor creates a layer of 'size' neurons with the appropriate number of dendrites
-	public NLayer(int size, int numDendrites) {
-		
+	private int numDendrites;
+
+	// constructor creates a layer of 'size' neurons with the appropriate number of dendrites
+	public NLayer(int size, int numD, boolean isOut) {
+
 		neurons = new Neuron[size];
-		for(int i = 0; i < numDendrites; i++) {
+		numDendrites = numD;
+		for (int i = 0; i < size; i++) {
 			neurons[i] = new Neuron(numDendrites);
 		}
-		
+
 	}
-	
-	//evaluates the layer, returning each neuron's values
+
+	// evaluates the layer, returning each neuron's values
 	public double[] evaluate(double input[]) {
-		
-		//check for appropriately sized input
-		if(input.length != neurons.length)
+
+		// check for appropriately sized input
+		if (input.length != neurons[0].getNumDendrites())
 			return null;
-		
+
 		double output[] = new double[neurons.length];
-		
-		//fire each neuron and get activation levels
-		for(int i = 0; i < neurons.length; i++) {
+
+		// fire each neuron and get activation levels
+		for (int i = 0; i < neurons.length; i++) {
 			neurons[i].evaluate(input);
 			output[i] = neurons[i].getActivationLevel();
 		}
-		
+
 		return output;
 	}
-	
+
 	public Neuron getNeuron(int i) {
 		return neurons[i];
 	}
-	
+
 	public int getSize() {
 		return neurons.length;
 	}
 	
+	public int getNumDendrites() {
+		return numDendrites;
+	}
+
 	public void setNeuron(Neuron n, int i) {
 		neurons[i] = n;
 	}
 	
-	public String toString() {
-		
-		String ret = "";
-		for(int i = 0; i < neurons.length; i++) {
-			ret.concat(neurons[i].toString());
-			if(i < neurons.length - 1)
-				ret.concat(", ");
-		}
-		return ret;
-		
+	public void setWeight(double w, int n, int i) {
+		neurons[n].setWeight(w, i);
 	}
 	
+	public void setBias(double b, int n) {
+		neurons[n].setBias(b);
+	}
+
+	public String toString() {
+
+		String ret = "";
+		for (int i = 0; i < neurons.length; i++) {
+			ret += neurons[i].toString();
+			if (i < neurons.length - 1)
+				ret += "\n";
+		}
+		return "" + ret;
+
+	}
+
 }
